efa1a4e902d069314bb2d026148a7037d0fb5d44
samyakahuja <samyak.mcs18.du@gmail.com>
2020-07-23 17:42:36 +0530

    prompt for git stuff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use git2::{
     Repository,
-    Delta,
+    Status,
 };
-//use std::path::Path;
 
 #[cfg(test)]
 mod tests;
@@ -15,44 +14,121 @@ fn main() {
         Err(e) => panic!("Repo not found {}", e)
     };
     println!("{:?}", repo.workdir().unwrap());
-    let prompt = Prompt::new(repo).unwrap();
-    println!("{}", prompt.git_status().unwrap());
-
+    let prompt = GitPrompt::new(repo).unwrap();
+    let config = PromptConfig::default();
+    println!("{}", prompt.show(&config));
 }
 
-pub struct Prompt {
+pub struct PromptConfig {
+    git_status_clean_symbol: &'static str,
+    git_status_unstaged_symbol: &'static str,
+    git_status_staged_symbol: &'static str,
+}
+
+impl Default for PromptConfig {
+    fn default() -> Self {
+        Self {
+            git_status_clean_symbol: "✓",
+            git_status_unstaged_symbol: "!",
+            git_status_staged_symbol: "+",
+        }
+    }
+}
+
+pub struct GitPrompt {
     repo: Repository,
 }
 
-impl Prompt {
+#[derive(Debug, PartialEq, Eq)]
+pub enum GitStatus {
+    Clean,
+    Unstaged,
+    Staged,
+}
+
+impl GitPrompt {
     pub fn new(repo: Repository) -> Result<Self> {
-        Ok(Prompt { repo })
+        Ok(GitPrompt { repo })
     }
 
-    pub fn git_status(&self) -> Option<String> {
-        let mut out = String::new();
-        let mut seen = Vec::new();
-
-        for status in self.repo.statuses(None).unwrap().iter() {
-            if let Some(status) = status.index_to_workdir() {
-                let delta = status.status();
-
-                if seen.contains(&delta) {
-                    continue;
+    pub fn branch(&self) -> Option<String> {
+        let head = match self.repo.head() {
+            Ok(reference) => reference,
+            Err(err) => {
+                dbg!(err.code());
+                return if err.code() == git2::ErrorCode::UnbornBranch {
+                    // read the branch name from .git/HEAD
+                    let head_path = self.repo.path().join("HEAD");
+                    let file_contents = std::fs::read_to_string(head_path).ok()?;
+                    Some(file_contents.lines().next()?.split("/").last()?.to_string())
+                } else {
+                    None
                 }
-                seen.push(delta);
+            }
+        };
 
-                match delta {
-                    Delta::Unmodified | Delta::Ignored | Delta::Copied | Delta:: Unreadable => {},
-                    Delta::Untracked => out.push('?'),
-                    Delta::Added => out.push('+'),
-                    Delta::Deleted => out.push('✘'),
-                    Delta::Modified | Delta::Renamed | Delta::Typechange => out.push('!'),
-                    Delta::Conflicted => out.push('#'),
+        let shorthand = head.shorthand();
+        shorthand.map(std::string::ToString::to_string)
+    }
+
+    pub fn head_reference_hash(&self) -> Option<String> {
+        let oid = self.repo.head().ok()?.target()?;
+        let oid_12 = oid.to_string()[1..13].to_string();
+        Some(oid_12)
+    }
+
+    pub fn git_status(&self) -> Option<GitStatus> {
+        let mut repo_status = GitStatus::Clean;
+
+        for file_status in self.repo.statuses(None).ok()?.iter() {
+            match file_status.status() {
+                // changes in working dir relative to Index
+                Status::WT_NEW
+                | Status::WT_DELETED
+                | Status::WT_RENAMED
+                | Status::WT_MODIFIED
+                | Status::WT_TYPECHANGE => {
+                    repo_status = GitStatus::Unstaged;
+                },
+                // changes in index relative to the head
+                Status::INDEX_NEW
+                | Status::INDEX_DELETED
+                | Status::INDEX_RENAMED
+                | Status::INDEX_MODIFIED
+                | Status::INDEX_TYPECHANGE => {
+                    repo_status = GitStatus::Staged;
                 }
+                _ => {}
             }
         }
 
-        if out.is_empty() { None } else { Some(out) }
+        Some(repo_status)
+    }
+
+    pub fn show(&self, config: &PromptConfig) -> String {
+        let branch = self.branch();
+        let status = self.git_status();
+
+        let hash = match branch {
+            Some(_) => self.head_reference_hash(),
+            None => None
+        };
+
+        let status_symbol = match status {
+            Some(status) => match status {
+                GitStatus::Clean => config.git_status_clean_symbol,
+                GitStatus::Unstaged => config.git_status_unstaged_symbol,
+                GitStatus::Staged => config.git_status_staged_symbol,
+            },
+            None => ""
+        };
+
+        match branch {
+            Some(branch) => match hash {
+                Some(hash) => format!("[{}:{} {}]", branch, hash, status_symbol),
+                None => format!("[{} {}]", branch, status_symbol),
+            },
+            None => format!("[{}]", status_symbol)
+        }
     }
 }
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -6,30 +6,31 @@ use std::path::PathBuf;
 use std::fs::{File, remove_file};
 use std::io::Write;
 //use test_helpers::*;
-use crate::Prompt;
+use crate::{
+    GitPrompt,
+    GitStatus,
+    PromptConfig
+};
 
 #[test]
-fn prompt_git_status() {
+fn git_prompt() {
     let temp_dir = TempDir::default();
     let temp_dir_path = PathBuf::from(temp_dir.as_ref());
 
     // Init repo
     let repo = Repository::init(&temp_dir_path).unwrap();
-    let prompt = Prompt::new(repo).unwrap();
-    assert_eq!(prompt.git_status(), None);
+    let prompt = GitPrompt::new(repo).unwrap();
+    assert_eq!(prompt.git_status().unwrap(), GitStatus::Clean);
 
     // Create file
     let file_name_01 = "test_file_01";
     let temp_file_path_01 = temp_dir_path.join(PathBuf::from(file_name_01));
-    let mut file_01 = File::create(&temp_file_path_01).unwrap();
-    assert_eq!(prompt.git_status().unwrap(), "?".to_string());
+    File::create(&temp_file_path_01).unwrap();
+    assert_eq!(prompt.git_status().unwrap(), GitStatus::Unstaged);
 
     // Add file
     let mut index = prompt.repo.index().unwrap();
-
-    assert_eq!(index.len(), 0);
     index.add_path(PathBuf::from(file_name_01).as_path()).unwrap();
-    assert_eq!(index.len(), 1);
 
     let index_entry_paths = index
         .iter()
@@ -39,28 +40,5 @@ fn prompt_git_status() {
         .collect::<Vec<_>>();
 
     assert_eq!(index_entry_paths, [file_name_01]);
-    assert_eq!(prompt.git_status(), None);
-
-    // Modify existing file
-    file_01.write_all(b"Hello, World!").unwrap();
-    assert_eq!(prompt.git_status().unwrap(), "!".to_string());
-
-    // Create new file
-    let file_name_02 = "test_file_02";
-    let temp_file_path_02 = temp_dir_path.join(PathBuf::from(file_name_02));
-    File::create(&temp_file_path_02).unwrap();
-    assert!(prompt.git_status().unwrap().contains("?"));
-    assert!(prompt.git_status().unwrap().contains("!"));
-
-    // Add both files
-    index.add_path(PathBuf::from(file_name_01).as_path()).unwrap();
-    assert!(!prompt.git_status().unwrap().contains("!"));
-    index.add_path(PathBuf::from(file_name_02).as_path()).unwrap();
-    assert_eq!(prompt.git_status(), None);
-
-    // Delete file
-    remove_file(temp_file_path_02).unwrap();
-    assert!(prompt.git_status().unwrap().contains("✘"));
-    index.remove_path(PathBuf::from(file_name_02).as_path()).unwrap();
-    assert_eq!(prompt.git_status(), None);
+    assert_eq!(prompt.git_status().unwrap(), GitStatus::Staged);
 }
